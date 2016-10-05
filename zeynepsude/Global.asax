ba0e1d2bcd5ca014e12bb62d923bef50d4cbc288
ZeynepSudeOzdemir <zeyozdemir@stu.okan.edu.tr>
2016-10-05 11:59:28 +0300

    Add files via upload

---
+++ zeynepsude/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zeynepsude.MvcApplication" Language="C#" %>
---
+++ zeynepsude/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zeynepsude.Startup))]
+namespace zeynepsude
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
